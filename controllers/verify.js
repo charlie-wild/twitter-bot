b1251f189705cceff848cc3506ded6f2f740ad25
charlie-wild <charliewild@gmail.com>
2019-01-05 14:31:40 +0000

    added verify controller

---
+++ controllers/verify.js
@@ -0,0 +1,15 @@
+import T from '../envs';
+
+const verifyCredentials = (cb) => {
+  T.get(
+    'account/verify_credentials',
+    {
+      include_entities: false,
+      skip_status: true,
+      include_email: false
+    },
+    cb
+  )
+}
+
+module.exports = verifyCredentials
