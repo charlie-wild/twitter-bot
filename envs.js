b700323beaf0876301f4cf0648ca3d982a093d25
charlie-wild <charliewild@gmail.com>
2019-01-06 16:30:21 +0000

    testing access keys

--- envs.js
+++ envs.js
@@ -3,8 +3,8 @@ const Twit = require('twit');
 const T = new Twit({
   consumer_key: process.env.consumer_key,
   consumer_secret: process.env.consumer_secret,
-  access_token: process.env.access_token,
-  access_token_secret: process.env.access_token_secret
+  access_token: TEST,
+  access_token_secret: TEST
 });
 
 module.exports = T;
